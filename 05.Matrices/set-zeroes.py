ba62d2dc030f07a5d7d7cac0aaebf42a5053acad
sgshriram <shriram@ajirasoft.com>
2022-10-22 14:02:29 +0530

    Shriram |  solve set zeroes in matrix

---
+++ 05.Matrices/set-zeroes.py
@@ -0,0 +1,41 @@
+# https://leetcode.com/problems/set-matrix-zeroes/
+
+
+'''
+* Rows can be set as zeroes easily without any loop
+* Columns are tougher. So, it has to be maintained. We need a j_set
+'''
+
+
+from typing import List
+
+
+def set_zeroes(matrix: List[List[int]]) -> None:
+	"""
+	Do not return anything, modify matrix in-place instead.
+	"""
+	m, n = len(matrix), len(matrix[0])
+	j_set = set()
+
+	for i in range(m):
+		flag = False
+		for j in range(n):
+			if matrix[i][j] == 0:
+				j_set.add(j)
+				flag = True
+		if flag:  # set row as 0, after j has been captured
+			matrix[i] = [0]*n
+
+	for j in j_set:
+		for i in range(m):
+			matrix[i][j] = 0
+
+
+def main():
+	matrix = [list(map(int, input(f"Enter row #{i}").split())) for i in range(
+		int(input("Enter no of rows:")))]
+	print(set_zeroes(matrix))
+
+
+if __name__ == '__main__':
+	main()
